e8d82320f1361175cf40d7436a961f584ab77881
Viktor Suchý <susenky.opavia@gmail.com>
2025-03-15 06:53:27 +0100

    nacitat tlacitka z db

--- src/web/app/dashboard/configuration/page.tsx
+++ src/web/app/dashboard/configuration/page.tsx
@@ -1,13 +1,18 @@
 import { ImageCard } from "~/components/image-card";
+import { prisma } from "~/modules/prisma";
 
-const items = Array.from(Array(9).keys());
+export default async function Page() {
+	const buttons = await prisma.button.findMany({
+		include: {
+			files: true,
+		},
+	});
 
-export default function Page() {
 	return (
 		<div className="flex flex-1 flex-col gap-4 px-4 py-10 pb-0">
 			<div className="grid grid-cols-3 gap-8">
-				{items.map((item) => (
-					<ImageCard key={item} id={item} />
+				{buttons.map((item) => (
+					<ImageCard key={item.id} item={item} />
 				))}
 			</div>
 		</div>
--- src/web/components/image-card.tsx
+++ src/web/components/image-card.tsx
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { Edit, Volume2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,20 +6,26 @@ import { Button } from "~/components/ui/button";
 import { Card, CardHeader, CardContent } from "~/components/ui/card";
 import { Separator } from "~/components/ui/separator";
 
-interface ImageCardProps {
-	id: number;
-}
+export function ImageCard({
+	item: { id, text, files },
+}: {
+	item: Prisma.ButtonGetPayload<{ include: { files: true } }>;
+}) {
+	const image = files.find((file) => file.type === "IMAGE");
 
-export function ImageCard({ id }: ImageCardProps) {
+	const voice = files.find((file) => file.type === "VOICE");
+
 	return (
 		<Card>
 			<CardHeader className="flex justify-center">
-				<Image
-					src="https://img.privezemenakup.cz/images/Rohl%C3%ADk%20tukov%C3%BD%2043g.jpg"
-					width={250}
-					height={100}
-					alt="rohlik"
-				/>
+				{image && (
+					<Image
+						src="https://img.privezemenakup.cz/images/Rohl%C3%ADk%20tukov%C3%BD%2043g.jpg"
+						width={250}
+						height={100}
+						alt="rohlik"
+					/>
+				)}
 			</CardHeader>
 
 			<Separator />
@@ -26,10 +33,10 @@ export function ImageCard({ id }: ImageCardProps) {
 			<CardContent>
 				<div className="flex flex-col gap-4">
 					<div className="flex justify-between items-center">
-						<p className="text-lg">Přidat</p>
+						<p className="text-lg">{text}</p>
 
 						<div className="flex gap-1">
-							<Button variant="outline">
+							<Button variant="outline" disabled={!voice}>
 								<Volume2 />
 							</Button>
 
