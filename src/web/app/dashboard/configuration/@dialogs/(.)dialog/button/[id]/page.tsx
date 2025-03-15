7066dfc757ee90ea8ad9ad7582767bee3c1cbd1b
Viktor Suchý <susenky.opavia@gmail.com>
2025-03-15 03:45:51 +0100

    shell dialogu na upravu tlacitka

--- src/web/app/dashboard/configuration/@dialogs/(.)dialog/button/[id]/page.tsx
+++ src/web/app/dashboard/configuration/@dialogs/(.)dialog/button/[id]/page.tsx
@@ -1,19 +1,38 @@
 import { DialogWrapper } from "~/components/dialog-wrapper";
-import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "~/components/ui/dialog";
+import { Button } from "~/components/ui/button";
+import { DialogContent, DialogHeader, DialogTitle } from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { VoiceRecorder } from "~/components/voice-recorder";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
 	const { id } = await params;
 
 	return (
 		<DialogWrapper>
-			<DialogContent>
+			<DialogContent className="">
 				<DialogHeader>
-					<DialogTitle>{id}</DialogTitle>
-					<DialogDescription>
-						This action cannot be undone. This will permanently delete your account and remove your data
-						from our servers.
-					</DialogDescription>
+					<DialogTitle>Úprava tlačítka {id}</DialogTitle>
 				</DialogHeader>
+
+				<div className="flex flex-col gap-4">
+					<div className="grid w-full items-center gap-1.5">
+						<Label htmlFor="picture">Text</Label>
+						<Input name="text" placeholder="Text tlačítka" />
+					</div>
+
+					<div className="grid w-full items-center gap-1.5">
+						<Label htmlFor="picture">Obrázek</Label>
+						<Input id="picture" type="file" />
+					</div>
+
+					<div className="grid w-full items-center gap-1.5">
+						<Label htmlFor="picture">Zvuk</Label>
+						<VoiceRecorder />
+					</div>
+				</div>
+
+				<Button>Uložit</Button>
 			</DialogContent>
 		</DialogWrapper>
 	);
