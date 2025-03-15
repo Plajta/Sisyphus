e678d91ab1960caabb77c002334a482714a8e83c
Viktor Suchý <susenky.opavia@gmail.com>
2025-03-15 01:30:16 +0100

    highlight veci v menu

--- src/web/components/nav-main.tsx
+++ src/web/components/nav-main.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { type LucideIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "~/components/ui/sidebar";
 
@@ -14,11 +15,13 @@ export function NavMain({
 		isActive?: boolean;
 	}[];
 }) {
+	const pathname = usePathname();
+
 	return (
 		<SidebarMenu className="pl-2">
 			{items.map((item) => (
 				<SidebarMenuItem key={item.title}>
-					<SidebarMenuButton asChild isActive={item.isActive}>
+					<SidebarMenuButton asChild isActive={item.url === pathname}>
 						<a href={item.url}>
 							<item.icon />
 							<span>{item.title}</span>
