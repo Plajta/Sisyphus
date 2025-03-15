eba6e118fc6cea24b1aec3ae1162b1fc22f2e9ce
Sles <slesusagi@mail.com>
2025-03-15 10:31:49 +0000

    jsem hloupy kluk

--- src/hardware/sd_card_reader/build/CMakeFiles/sd_card_reader.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/sd_card_reader.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.o: \
- /home/Sles/pr/pico/sd_card_reader/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c \
- /home/Sles/pr/pico/sd_card_reader/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.h \
- /usr/lib/gcc/arm-none-eabi/14.2.0/include/stddef.h
--- src/hardware/sd_card_reader/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/Sles/pr/pico/sd_card_reader/build/_deps/picotool/"
-};
