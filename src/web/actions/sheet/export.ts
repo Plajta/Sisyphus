2c58ac9fb7b8b660ba5ee0d230c6cd0638d51f22
Viktor Suchý <susenky.opavia@gmail.com>
2025-03-16 03:53:26 +0100

    pres parametr generovast

--- src/web/actions/sheet/export.ts
+++ src/web/actions/sheet/export.ts
@@ -56,21 +56,51 @@ function create2DArrayFromArray<T>(arr: T[]): T[][] {
 }
 
 const sheetDimensions = {
-	width: mmsToPoints(138),
-	height: mmsToPoints(160),
-	buttonGridHeight: mmsToPoints(140),
-	sectionDimensions: mmsToPoints(42),
-	colorSectionHeight: mmsToPoints(10),
-	buttonGridGap: mmsToPoints(3),
-	gridPadding: mmsToPoints(10),
+	plajta: {
+		sheetNameText: {
+			x: offsetPoints(mmsToPoints(2)),
+			y: offsetPoints(mmsToPoints(143)),
+		},
+		authorNameText: {
+			x: offsetPoints(mmsToPoints(90)),
+			y: offsetPoints(mmsToPoints(143)),
+		},
+		foldText: {
+			x: offsetPoints(mmsToPoints(3)),
+			y: offsetPoints(mmsToPoints(153)),
+		},
+		container: {
+			x: offsetPoints(0),
+			y: offsetPoints(0),
+			width: mmsToPoints(138),
+			height: mmsToPoints(160),
+		},
+		headerDivider: {
+			gap: mmsToPoints(10),
+		},
+		button: {
+			width: mmsToPoints(42),
+			height: mmsToPoints(42),
+		},
+		buttonGrid: {
+			width: mmsToPoints(138),
+			height: mmsToPoints(140),
+			gap: mmsToPoints(3),
+		},
+		buttonGridHeight: mmsToPoints(140),
+		sectionDimensions: mmsToPoints(42),
+		colorSectionHeight: mmsToPoints(10),
+		buttonGridGap: mmsToPoints(3),
+		gridPadding: mmsToPoints(10),
+	},
 };
 
 const schema = z.object({
 	id: z.coerce.number(),
-	//type: z.enum(["PLAJKOM", "FT12"]),
+	type: z.enum(["plajta", "ft12"]),
 });
 
-export const exportSheet = actionClient.schema(schema).action(async ({ parsedInput: { id } }) => {
+export const exportSheet = actionClient.schema(schema).action(async ({ parsedInput: { id, type } }) => {
 	const sheet = await prisma.sheet.findFirst({
 		where: {
 			id,
@@ -97,77 +127,77 @@ export const exportSheet = actionClient.schema(schema).action(async ({ parsedInp
 
 	const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-	const text = "© Plajta 2025";
+	const authorText = "Sisyphus-gen 2025";
 	const fontSize = 15;
-
-	page.drawText(sheet.name.normalize("NFD").replace(/[\u0300-\u036f]/g, ""), {
-		x: offsetPoints(mmsToPoints(2)),
-		y: offsetPoints(mmsToPoints(143)),
-		size: fontSize,
-		font: font,
-		color: rgb(0, 0, 0),
-	});
-
-	page.drawText(text, {
-		x: offsetPoints(mmsToPoints(95)),
-		y: offsetPoints(mmsToPoints(143)),
-		size: fontSize,
-		font: font,
-		color: rgb(0, 0, 0),
-	});
 
 	const foldText = "-- Tudle vohnout --";
 
-	page.drawText(foldText, {
-		x: offsetPoints(mmsToPoints(2)),
-		y: offsetPoints(mmsToPoints(153)),
+	const dimensions = sheetDimensions["plajta"];
+
+	page.drawText(sheet.name.normalize("NFD").replace(/[\u0300-\u036f]/g, ""), {
+		...dimensions.sheetNameText,
 		size: fontSize,
 		font: font,
 		color: rgb(0, 0, 0),
 	});
 
-	page.drawText(foldText, {
-		x: offsetPoints(mmsToPoints(92)),
-		y: offsetPoints(mmsToPoints(153)),
+	page.drawText(authorText, {
+		...dimensions.authorNameText,
 		size: fontSize,
 		font: font,
 		color: rgb(0, 0, 0),
 	});
 
-	page.drawLine({
-		start: { x: offsetPoints(0), y: offsetPoints(sheetDimensions.buttonGridHeight) },
-		end: { x: offsetPoints(sheetDimensions.width), y: offsetPoints(sheetDimensions.buttonGridHeight) },
-		thickness: 1,
-		color: rgb(0, 0, 0),
-	});
+	if (dimensions.foldText) {
+		page.drawText(foldText, {
+			...dimensions.foldText,
+			size: fontSize,
+			font: font,
+			color: rgb(0, 0, 0),
+		});
 
-	page.drawLine({
-		start: { x: offsetPoints(0), y: offsetPoints(sheetDimensions.buttonGridHeight + mmsToPoints(10)) },
-		end: {
-			x: offsetPoints(sheetDimensions.width),
-			y: offsetPoints(sheetDimensions.buttonGridHeight + mmsToPoints(10)),
-		},
-		thickness: 1,
-		color: rgb(0, 0, 0),
-	});
+		page.drawText(foldText, {
+			x: dimensions.foldText.x + 2 * dimensions.sectionDimensions + 15,
+			y: dimensions.foldText.y,
+			size: fontSize,
+			font: font,
+			color: rgb(0, 0, 0),
+		});
+	}
+
+	if (dimensions.headerDivider) {
+		page.drawLine({
+			start: { x: offsetPoints(0), y: offsetPoints(dimensions.buttonGrid.height) },
+			end: { x: offsetPoints(dimensions.buttonGrid.width), y: offsetPoints(dimensions.buttonGrid.height) },
+			thickness: 1,
+			color: rgb(0, 0, 0),
+		});
+
+		page.drawLine({
+			start: { x: offsetPoints(0), y: offsetPoints(dimensions.buttonGrid.height + dimensions.headerDivider.gap) },
+			end: {
+				x: offsetPoints(dimensions.buttonGrid.width),
+				y: offsetPoints(dimensions.buttonGrid.height + dimensions.headerDivider.gap),
+			},
+			thickness: 1,
+			color: rgb(0, 0, 0),
+		});
+
+		const sheetColor = hexToRgb(sheet.colorCode)!;
+
+		page.drawRectangle({
+			x: offsetPoints(dimensions.button.width + dimensions.buttonGrid.gap * 2),
+			y: offsetPoints(dimensions.buttonGrid.height + mmsToPoints(10)),
+			height: dimensions.headerDivider.gap,
+			width: dimensions.button.width,
+			color: rgb(sheetColor.r / 255, sheetColor.g / 255, sheetColor.b / 255),
+		});
+	}
 
 	page.drawRectangle({
-		x: offsetPoints(0),
-		y: offsetPoints(0),
-		width: sheetDimensions.width,
-		height: sheetDimensions.height,
+		...dimensions.container,
 		borderWidth: 1,
 		borderColor: rgb(0, 0, 0),
-	});
-
-	const sheetColor = hexToRgb(sheet.colorCode)!;
-
-	page.drawRectangle({
-		x: offsetPoints(sheetDimensions.sectionDimensions + sheetDimensions.buttonGridGap * 2),
-		y: offsetPoints(sheetDimensions.buttonGridHeight + mmsToPoints(10)),
-		height: sheetDimensions.colorSectionHeight,
-		width: sheetDimensions.sectionDimensions,
-		color: rgb(sheetColor.r / 255, sheetColor.g / 255, sheetColor.b / 255),
 	});
 
 	const button2DArray = create2DArrayFromArray(sheet.buttons);
@@ -186,23 +216,16 @@ export const exportSheet = actionClient.schema(schema).action(async ({ parsedInp
 
 			page.drawImage(image, {
 				x: offsetPoints(
-					sheetDimensions.buttonGridGap +
-						buttonIndex * (sheetDimensions.buttonGridGap + sheetDimensions.sectionDimensions)
+					dimensions.buttonGrid.gap + buttonIndex * (dimensions.buttonGrid.gap + dimensions.button.width)
 				),
-				y: offsetPoints(
-					mmsToPoints(20) + rowIndex * (sheetDimensions.buttonGridGap + sheetDimensions.sectionDimensions)
-				),
-				height: sheetDimensions.sectionDimensions - 50,
-				width: sheetDimensions.sectionDimensions,
+				y: offsetPoints(mmsToPoints(20) + rowIndex * (dimensions.buttonGrid.gap + dimensions.button.width)),
+				height: dimensions.button.height - 50,
+				width: dimensions.button.width,
 			});
 
 			page.drawText(button.text.toUpperCase(), {
-				x: offsetPoints(
-					mmsToPoints(10) + buttonIndex * (sheetDimensions.buttonGridGap + sheetDimensions.sectionDimensions)
-				),
-				y: offsetPoints(
-					mmsToPoints(10) + rowIndex * (sheetDimensions.buttonGridGap + sheetDimensions.sectionDimensions)
-				),
+				x: offsetPoints(mmsToPoints(10) + buttonIndex * (dimensions.buttonGrid.gap + dimensions.button.width)),
+				y: offsetPoints(mmsToPoints(10) + rowIndex * (dimensions.buttonGrid.gap + dimensions.button.height)),
 				size: 15,
 				font: font,
 				color: rgb(0, 0, 0),
